54f0f11a2da0b10f23be23168a0c0fb194d314bc
Ali Mohsen <3li.mo7sen.m7moud@gmail.com>
2026-04-21 14:08:43 +0200

    Add My Portfolio Project to Git

---
+++ Core/Interfaces/IPortfolioRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces
+{
+    public interface IPortfolioRepository
+    {
+        Task<IReadOnlyList<PortfolioItem>> GetAllAsync();
+        Task<PortfolioItem?> GetByIdAsync(int id);
+        void AddItem(PortfolioItem item);
+        void UpdateItem(PortfolioItem item);
+        void DeleteItem(PortfolioItem item);
+        Task<bool> SaveAllChangesAsync();
+    }
+}
---
+++ Infrastructure/Data/PortfolioContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core.Entities;
+
+namespace Infrastructure.Data
+{
+    public class PortfolioContext : DbContext
+    {
+        public PortfolioContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<PortfolioItem> PortfolioItems { get; set; }
+    }
+}
